bc6c4ce95e8a8eea21e19ca98e46b8a4aa64df8a
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-27 12:37:09 +0530

    [Elavarasu] UC2-Calculate daily employee wage

--- employee_wage.py
+++ employee_wage.py
@@ -3,13 +3,17 @@ import random
 
 def wage_computation():
     """
-    This function used to find employee status present or absent
+    This function computes wage of an employee
     :return: None
     """
+    employee_wage_per_hour = 20
+    is_full_time = 1
+    full_time_hour = 8
     employee_status = random.randint(0, 1)
-
-    if employee_status == 1:
+    if employee_status == is_full_time:
         print("Employee is Present")
+        employee_wage = full_time_hour * employee_wage_per_hour
+        print("Employee Wage for a Day: ", employee_wage)
     else:
         print("Employee is Absent")
 
